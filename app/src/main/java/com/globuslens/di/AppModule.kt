6861538ed7bed7717486a751125bad803e95d374
Mathew <matthewcharles.tech@gmail.com>
2026-03-06 14:20:25 +0530

    Dagger Hilt Modules

---
+++ app/src/main/java/com/globuslens/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.globuslens.di
+
+import com.globuslens.utils.NetworkUtils
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import android.content.Context
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNetworkUtils(@ApplicationContext context: Context): NetworkUtils {
+        return NetworkUtils(context)
+    }
+}
